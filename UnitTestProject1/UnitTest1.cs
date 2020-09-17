65f67511c76e3010dedb9e474f8510f5f8085edb
unknown <kumar.abhishek11718@gmail.com>
2020-09-17 11:05:04 -0700

    Add this to remote repo

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿ using System;
+ using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication1;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DemoClass obj = new DemoClass();
+            Assert.AreEqual("Abhishek", obj.fname);
+            Assert.AreEqual("Kumar", obj.lname);
+        }
+    }
+}
---
+++ WebApplication1/DemoClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class DemoClass
+    {
+        public string fname;
+        public string lname;
+        public DemoClass()
+        {
+            this.fname = "Abhishek";
+            this.lname = "Kumar";
+        }
+    }
+}
